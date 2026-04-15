7ff6f51c1475d8cce37330daf41b6d49a0ca1f3e
russel-fullstack <russeldev95@gmail.com>
2026-04-15 14:30:04 +0100

    prepare and recuperation on  the results

--- 02-mysqlphp/index.php
+++ 02-mysqlphp/index.php
@@ -2,6 +2,18 @@
 
 include 'database.php';
 
+$sql= "SELECT * FROM etudiants";
+
+$req = $pdo->prepare($sql);
+
+$req->execute();
+
+$etudiants = $req->fetchAll();
+
+// echo "<pre>";
+// var_dump($etudiants);
+// echo "</pre>";
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +24,28 @@ include 'database.php';
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Liste des étudiants</h1>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($etudiants as $etudiant): ?>
+            <tr>
+                <td><?= htmlspecialchars($etudiant['id']) ?></td>
+                <td><?= htmlspecialchars($etudiant['nom']) ?></td>
+                <td><?= htmlspecialchars($etudiant['prenom']) ?></td>
+                <td><?= htmlspecialchars($etudiant['email']) ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
 </body>
 </html>
