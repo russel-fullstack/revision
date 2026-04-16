b7c56792b0fde1bdd11dc5bd638661bbf26a0ddb
russel-fullstack <russeldev95@gmail.com>
2026-04-16 14:04:39 +0100

    tp1_crud_backend_termine

--- 02-mysqlphp/create.php
+++ 02-mysqlphp/create.php
@@ -9,18 +9,38 @@ if(isset($_POST['soumettre'])){
     $nom = htmlspecialchars($_POST['nom']);
     $prenom = htmlspecialchars($_POST['prenom']);
     $email = htmlspecialchars($_POST['email']);
+    $pseudo = htmlspecialchars($_POST['pseudo']);
 
-    if(!empty($nom) || !empty($prenom) || !empty($email)){
-        $sql = "INSERT INTO etudiants (nom, prenom, email) VALUES (:nom, :prenom, :email)";
+    if(!empty($nom) || !empty($prenom) || !empty($email) || !empty($pseudo)){
+    $sql_email = "SELECT * FROM etudiants WHERE email = ?";
+    $check_email = $pdo->prepare($sql_email);
+    $check_email->execute([$email]);
+    $email_exist = $check_email->fetchColumn();
+
+    $sql_pseudo = "SELECT * FROM etudiants WHERE pseudo = ?";
+    $check_pseudo = $pdo->prepare($sql_pseudo);
+    $check_pseudo->execute([$pseudo]);
+    $pseudo_exist = $check_pseudo->fetchColumn();
+    
+    if($pseudo_exist){
+        $message = '<p style="color: red;">Le pseudo est déjà utilisée .</p>';
+    }elseif($email_exist){
+        $message = '<p style="color: red;">L\'email est déjà utilisée .</p>';
+    }else{
+
+
+        $sql = "INSERT INTO etudiants (nom, prenom, pseudo, email) VALUES (:nom, :prenom, :pseudo, :email)";
         $req = $pdo->prepare($sql);
         $req ->execute([
             ':nom' => $nom,
             ':prenom' => $prenom,
+            ':pseudo' => $pseudo,
             ':email' => $email
         ]);
         $_SESSION['message'] = "Création du nouvel étudiant réussie !";
         header("Location: index.php");
         exit();
+    }
     } else {
         $message = "Veuillez remplir tous les champs.";
     }
@@ -53,6 +73,11 @@ if(isset($_POST['soumettre'])){
         <div>
             <label>Prénom :</label><br>
             <input type="text" name="prenom" >
+        </div>
+        <br>
+        <div>
+            <label>Pseudo:</label><br>
+            <input type="text" name="pseudo" >
         </div>
         <br>
         <div>
--- 02-mysqlphp/index.php
+++ 02-mysqlphp/index.php
@@ -1,40 +1,70 @@
 <?php
-declare(strict_types=1);
-session_start();
+//  declare(strict_types=1);
+session_start();  // Indispensable pour utiliser les sessions
+include "database.php";
 
-include 'database.php';
+// Initialisation de la variable de recherche
+$search = $_GET['search'] ??'';
+// 0. Base de la requête
+$sql = "SELECT * FROM etudiants";
+$params =[];
 
-$sql= "SELECT * FROM etudiants";
+// Si une recherche est lancée, on ajoute le WHERE
+if (!empty($search)) {
+    $sql .= " WHERE nom LIKE ? OR email LIKE ?";
+}
 
+// On ajoute TOUJOURS le tri à la fin
+$sql .= " ORDER BY id DESC";
+
+// 1. Préparation
 $req = $pdo->prepare($sql);
 
-$req->execute();
+// 2. Liaison du paramètre (uniquement si nécessaire)
+if (!empty($search)) {
+    $terme = '%' . $search . '%';
+   $params =[
+    $terme,
+    $terme
+   ];
+}
 
+// 3. Exécution
+$req->execute( $params);
+
+// 4. Récupération
 $etudiants = $req->fetchAll();
-
-// echo "<pre>";
-// var_dump($etudiants);
-// echo "</pre>";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mysql php connect DB</title>
 </head>
+
 <body>
     <h1>Liste des étudiants</h1>
 
-    <?php if (isset($_SESSION['message'])): ?>
-        <p style="color: green; padding: 10px; background-color: #d4edda; border: 1px solid #c3e6cb;">
-            <?= $_SESSION['message'] ?>
-        </p>
-        <?php unset($_SESSION['message']); ?>
+    <?php if (isset($_SESSION['succes'])) : ?>
+        <p style="color: green; padding: 5px; background:rgba(0,255,0,0.3)"><?= $_SESSION['succes'] ?></p>
+        <?php unset($_SESSION['succes'])  ?>
     <?php endif; ?>
-    <a href="create.php">Ajouter un étudiant</a><br><br>
+    <!-- Formulaire de recherche -->
+    <form method="GET" action="" class="mb-4">
+        <a class="" href="create.php">Créer un
+            nouvel étudiant</a>
+        <a class=""
+            href="index.php">Actualiser
+        </a>
+        <input type="text" name="search" placeholder="Rechercher par nom ou email"
+            value="<?= $search ?>" class="">
+        <button type="submit" class="">Rechercher</button>
+    </form>
+
     <table border="1">
         <thead>
             <tr>
@@ -42,23 +72,31 @@ $etudiants = $req->fetchAll();
                 <th>Nom</th>
                 <th>Prénom</th>
                 <th>Email</th>
+                <th>Pseudo</th>
                 <th>Actions</th>
             </tr>
         </thead>
+
         <tbody>
             <?php foreach ($etudiants as $etudiant): ?>
-            <tr>
-                <td><?= $etudiant['id'] ?></td>
-                <td><?= htmlspecialchars($etudiant['nom']) ?></td>
-                <td><?= htmlspecialchars($etudiant['prenom']) ?></td>
-                <td><?= htmlspecialchars($etudiant['email']) ?></td>
-                <td>
-                    <a href="update.php?id=<?= $etudiant['id'] ?>" name="modifier">Edit</a>
-                    <a href="delete.php?id=<?= $etudiant['id'] ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet étudiant ?');">Delete</a>
-                </td>
-            </tr>   
+                <tr>
+                    <td><?= $etudiant['id'] ?></td>
+                    <td><?= htmlspecialchars($etudiant['nom']) ?></td>
+                    <td><?= htmlspecialchars($etudiant['prenom']) ?></td>
+                    <td><?= htmlspecialchars($etudiant['email']) ?></td>
+                    <td><?= htmlspecialchars($etudiant['pseudo']) ?></td>
+                    <td>
+                        <a href="update.php?id=<?= $etudiant['id'] ?>">Edit </a>
+                        <a href="delete.php?id=<?= $etudiant['id'] ?>" onClick=" return confirm('Êtes-vous sûr de vouloir supprimer ?')">
+                            Supprimer
+                        </a>
+
+                    </td>
+                </tr>
             <?php endforeach; ?>
         </tbody>
+
     </table>
 </body>
+
 </html>
